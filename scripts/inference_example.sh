a4a62357a253e6cbca5e02b3d08e7778a80a7efc
Ozel Yilmazel <ozel@yilmazel.com>
2026-02-06 15:21:07 +0000

    images

--- scripts/inference_example.sh
+++ scripts/inference_example.sh
@@ -8,8 +8,6 @@
 #SBATCH --mem=24G
 #SBATCH -C "vram40|vram48&sm_70|sm_75|sm_80|sm_86|sm_89|sm_90"
 #SBATCH --cpus-per-task=2
-#SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --mail-user=oyilmazel@umass.edu
 
 module load conda/latest
 module load cuda/12.6
@@ -22,3 +20,5 @@ conda activate lamp
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 python -u llm_service/inference_example.py
+
+# you can observe status of jobs with squeue --me
