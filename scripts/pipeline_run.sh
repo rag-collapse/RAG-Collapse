a6c47f4305d7d77f253299cd8348ccb104ac5295
Ozel Yilmazel <ozel@yilmazel.com>
2026-02-10 04:31:32 +0000

    moved pipeline entrypoint to root, cleaned sbatch

--- scripts/pipeline_run.sh
+++ scripts/pipeline_run.sh
@@ -9,37 +9,28 @@
 #SBATCH -C "vram40|vram48&sm_70|sm_75|sm_80|sm_86|sm_89|sm_90"
 #SBATCH --cpus-per-task=2
 
-set -e
-
 module load conda/latest
 conda activate ragenv
 
-python -m pip install -r requirements.txt
-
-# -------------------------
-# API mode (CLI args)
-# -------------------------
-export API_KEY= # set this to LiteLLM API key
- 
-python -m pipeline.run_pipeline \
-  --model-mode api \
-  --model-name openai/gpt4o \
-  --dataset-path datasets/umass_data.entity.chatgpt.50.jsonl \
-  --output-path experiments_output.json \
-  --max-questions 1 \
-  --num-iterations 5 \
-  --num-runs 10 \
-  --chars-per-doc 400
-
-# -------------------------
-# Local mode (uncomment to use)
-# -------------------------
-# python -m pipeline.run_pipeline \
-#   --model-mode local \
-#   --model-name Qwen/Qwen2.5-1.5B-Instruct \
+# python -u pipeline.py \
+#   --model-mode api \
+#   --model-name openai/gpt4o \
 #   --dataset-path datasets/umass_data.entity.chatgpt.50.jsonl \
-#   --output-path example_experiments_output.json \
+#   --output-path experiments_outputs/output.json \
 #   --max-questions 1 \
 #   --num-iterations 5 \
 #   --num-runs 10 \
 #   --chars-per-doc 400
+
+# -------------------------
+# Local mode (uncomment to use)
+# -------------------------
+python -u pipeline.py \
+  --model-mode local \
+  --model-name Qwen/Qwen2.5-1.5B-Instruct \
+  --dataset-path datasets/umass_data.entity.chatgpt.50.jsonl \
+  --output-path experiment_outputs/output.json \
+  --max-questions 8 \
+  --num-iterations 2 \
+  --num-runs 10 \
+  --chars-per-doc 400
