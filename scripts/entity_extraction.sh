f71e585ae01887233fc12e20cf2db769868fcf41
Riddhimaan-Senapati <rsenapati@umass.edu>
2026-02-19 15:15:54 -0500

    updated entity-extraction script

--- scripts/entity_extraction.sh
+++ scripts/entity_extraction.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 #SBATCH --job-name=entity-extraction
-#SBATCH --output=logs/pipeline_%A.out
-#SBATCH --error=logs/pipeline_%A.err
-#SBATCH --time=2:00:00
+#SBATCH --output=logs/entity-extraction_%A.out
+#SBATCH --error=logs/entity-extraction_%A.err
+#SBATCH --time=7:00:00
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --mem=32G
+#SBATCH --mem=24G
 #SBATCH -C "vram40|vram48&sm_70|sm_75|sm_80|sm_86|sm_89|sm_90"
 #SBATCH --cpus-per-task=2
 
 module load conda/latest
+module load cuda/12.6
+# activate conda environment
 conda activate ragenv
 
 # API mode(uncomment to use)
